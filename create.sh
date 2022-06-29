161d4926f785f84ff3ac28db72655af6dacffc52
jp112sdl <jp112sdl@icloud.com>
2022-06-29 16:03:38 +0200

    update

--- create.sh
+++ create.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-tar czf package/tools_macosx-2022.6.29.tar.gz tools
-tar czf package/tools_linux64-2022.6.29.tar.gz tools
-tar czf package/tools_win-2022.6.29.tar.gz tools
+tar czf package/efm32tools_macosx-2022.6.29.tar.gz tools
+tar czf package/efm32tools_linux64-2022.6.29.tar.gz tools
+tar czf package/efm32tools_win-2022.6.29.tar.gz tools
 cd hardware/arm
 tar czf ../../package/ARDUINO_EFM32-2022.6.29.tar.gz EFM32
 cd ../../
-sha256sum package/tools_macosx-2022.6.29.tar.gz
-sha256sum package/tools_linux64-2022.6.29.tar.gz
-sha256sum package/tools_win-2022.6.29.tar.gz
+sha256sum package/efm32tools_macosx-2022.6.29.tar.gz
+sha256sum package/efm32tools_linux64-2022.6.29.tar.gz
+sha256sum package/efm32tools_win-2022.6.29.tar.gz
 sha256sum package/ARDUINO_EFM32-2022.6.29.tar.gz
